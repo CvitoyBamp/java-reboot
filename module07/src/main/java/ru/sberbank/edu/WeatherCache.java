2c0eb77c1b4fbe6387a1b0dc5315772ffa953957
CvitoyBamp <cvitoybamp@yandex.ru>
2023-11-18 23:05:30 +0300

    module 7 init

--- module07/src/main/java/ru/sberbank/edu/WeatherCache.java
+++ module07/src/main/java/ru/sberbank/edu/WeatherCache.java
@@ -1,20 +1,23 @@
 package ru.sberbank.edu;
 
+import lombok.Getter;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.web.client.HttpClientErrorException;
+
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
 
+@Getter
 public class WeatherCache {
-
     private final Map<String, WeatherInfo> cache = new HashMap<>();
-    private final WeatherProvider weatherProvider;
+    WeatherProvider weatherProvider;
 
     /**
      * Constructor.
-     *
-     * @param weatherProvider - weather provider
      */
-    public WeatherCache(WeatherProvider weatherProvider) {
-        this.weatherProvider = weatherProvider;
+    public WeatherCache() {
+        this.weatherProvider = new WeatherProvider();
     }
 
     /**
@@ -26,15 +29,43 @@ public class WeatherCache {
      * @param city - city
      * @return actual weather info
      */
-    public WeatherInfo getWeatherInfo(String city) {
-        // should be implemented
-        return null;
+    public WeatherInfo getWeatherInfo(@NotNull String city) {
+
+        synchronized (cache) {
+            try {
+                WeatherInfo weatherInfo = weatherProvider.get(city);
+                if (weatherInfo != null) {
+                    cache.entrySet().stream().filter(val -> val.getKey().equals(city))
+                            .findAny()
+                            .map(Map.Entry::getValue)
+                            .ifPresentOrElse(
+                                    (wi) -> {
+                                        if (LocalDateTime.now().isAfter(wi.getExpiryTime())) {
+                                                cache.replace(city, weatherInfo);
+                                                System.out.println("Weather info for city " + city + " was updated.");
+                                        }
+                                    },
+                                    () -> {
+                                        cache.put(city, weatherInfo);
+                                        System.out.println("City " + city + " was added to cache.");
+                                    }
+                            );
+                }
+                return weatherInfo;
+            } catch (HttpClientErrorException ex) {
+                removeWeatherInfo(city);
+                return null;
+            }
+        }
     }
 
     /**
      * Remove weather info from cache.
      **/
     public void removeWeatherInfo(String city) {
-        // should be implemented
+        synchronized (cache) {
+            cache.remove(city);
+        }
     }
+
 }
